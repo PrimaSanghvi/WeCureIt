e11530b51103b7dd5763ff1d0688b29e5f1d5711
Lauren Hahn <62489998+Lhahn01@users.noreply.github.com>
2024-04-10 02:12:24 -0400

    Revert "Merge haidong_dev into ticket4.7 branch"

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -77,16 +77,24 @@ WSGI_APPLICATION = "backend.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
+import environ
+env = environ.Env()
+environ.Env.read_env()
+...
+# Your secret key
+SECRET_KEY = env("SECRET_KEY")
+
 DATABASES = {
-   'default': {
-       'ENGINE': 'django.db.backends.postgresql_psycopg2',
-       'NAME': 'wecuredata',
-       'USER': 'postgres',
-       'PASSWORD': 'Zhd123456@',
-       'HOST': '127.0.0.1',
-       'PORT': '5432',
-   }
+    'default': {
+     'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': env("DB_NAME"),
+        'USER': env("DB_USER"),
+        'PASSWORD': env("DB_PASSWORD"),
+        'HOST': env("DB_HOST"),
+        'PORT': env("DB_PORT"),
+    }
 }
+
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,8 +4,6 @@ import SignupPage from "./Components/LoginPage/SignupPage";
 import SignupPage2 from "./Components/LoginPage/SignupPage2";
 import {UserHomePage} from "./Components/Patient/UserHomePage";
 import {UserEditProfile} from "./Components/Patient/UserEditProfile";
-import FacilityHome from './Components/Admin/FacilityHome';
-import FacilityEdit from './Components/Admin/FacilityEdit';
 import { UserEditPreference, UserEditSaved } from './Components/Patient/UserEditPreference';
 import { BrowserRouter as Router, Route,Routes } from 'react-router-dom';
 
@@ -21,8 +19,6 @@ const App = () => {
         <Route path="/creditCardDetails" element={<SignupPage2 />}/>
         <Route path="/patientHomepage/:patientId" element={<UserHomePage />}/>
         <Route path="/editProfile/:patientId" element={<UserEditProfile />}/>
-        <Route path="/admin/facility" element={<FacilityHome />}/>
-        <Route path="/admin/facility/:facilityId" element={<FacilityEdit />}/>
         <Route path="/editPreference/:patientId" element={<UserEditPreference />}/>
       </Routes>
     </div>
