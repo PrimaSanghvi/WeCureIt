37cb631d74c691491aa2da306baedeaa4f471283
Lauren Hahn <62489998+Lhahn01@users.noreply.github.com>
2024-04-23 02:25:17 -0400

    create fake data

--- backend/scripts/deleteDB.py
+++ backend/scripts/deleteDB.py
@@ -2,12 +2,18 @@
 # Requirements:
 # 1. Need psycopg2 installed
 # 2. Need django-extensions installed
-from weCureIt.models import Doctor, ManageRooms, Facility, AdminTable, Speciality
+from weCureIt.models import Doctor, ManageRooms, Facility, AdminTable, Speciality, Patient, PatientCreditCard, PatientPreference, Doc_schedule, Patient_record, Appointments
 
 def run():
     # ################# DELETE ALL ITEMS FIRST FOR A CLEAN START #################
     ManageRooms.objects.all().delete()
+    Appointments.objects.all().delete()
     Facility.objects.all().delete()
+    Doc_schedule.objects.all().delete()
     Doctor.objects.all().delete()
     AdminTable.objects.all().delete()
     Speciality.objects.all().delete()
+    PatientCreditCard.objects.all().delete()
+    PatientPreference.objects.all().delete()
+    Patient_record.objects.all().delete()
+    Patient.objects.all().delete()
